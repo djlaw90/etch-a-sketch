fa4781f25533e69e5c913ab58695fc63db927ce3
djlaw90 <djlaw90@gmail.com>
2023-09-26 17:05:58 -0500

    stuck on slider not working

--- app.js
+++ app.js
@@ -6,16 +6,35 @@
 
 const grid = document.querySelector('.grid');
 
-for (let i = 0; i < 16; i++) {
-  for (let j = 0; j < 16; j++) {
-    const cell = document.createElement('div');
-    cell.classList.add('cell');
-    cell.addEventListener('mouseover', function(){
-        cell.style.backgroundColor = '#28282B';
-    });
-    grid.appendChild(cell);
-  }
+const rangeSlider = document.getElementById("myRange");
+const rangeValue = document.querySelector(".range-value");
+rangeValue.innerHTML = rangeSlider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+rangeSlider.oninput = function() {
+  rangeValue.innerHTML = this.value;
+  displayGrid(this.value);
 }
+
+
+const displayGrid = (value = 16) => {
+    grid.style.gridTemplateColumns = `repeat(${value}, auto)`;
+    grid.style.gridTemplateRows = `repeat(${value}, auto)`;
+    for (let i = 0; i < value; i++) {
+        for (let j = 0; j < value; j++) {
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            cell.addEventListener('mouseover', function(){
+                cell.style.backgroundColor = '#28282B';
+            });
+            grid.appendChild(cell);
+        }
+    }
+}
+
+displayGrid();
+
+
 
 
 
