ea007c92b9377b45bd3dc24ddeafc974d8d9a6de
djlaw90 <djlaw90@gmail.com>
2023-09-26 17:11:11 -0500

    stuck on slider not working some updates

--- app.js
+++ app.js
@@ -8,14 +8,13 @@ const grid = document.querySelector('.grid');
 
 const rangeSlider = document.getElementById("myRange");
 const rangeValue = document.querySelector(".range-value");
-rangeValue.innerHTML = rangeSlider.value; // Display the default slider value
 
-// Update the current slider value (each time you drag the slider handle)
-rangeSlider.oninput = function() {
-  rangeValue.innerHTML = this.value;
-  displayGrid(this.value);
-}
+rangeValue.innerHTML = rangeSlider.value;
 
+rangeSlider.addEventListener('input', function(){
+    rangeValue.innerHTML = this.value;
+    displayGrid(this.value);
+});
 
 const displayGrid = (value = 16) => {
     grid.style.gridTemplateColumns = `repeat(${value}, auto)`;
